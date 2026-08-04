512ecb2da47fdf45f5fe2dce20dad4254b3c86a6
890asif <192508892+890asif@users.noreply.github.com>
2026-08-04 14:57:46 +0600

    Inventory Merge & Max Score

---
+++ merge-max.js
@@ -0,0 +1,21 @@
+function mergeInventory(array1, array2){
+    if(!Array.isArray(array1, array2)){
+        return "invalid"
+        
+    }
+    return[...array1, ...array2]
+
+}
+
+function highestScore(scores){
+        if(!Array.isArray(scores)){
+            return "invalid"
+        }
+        return Math.max(...scores)
+    }
+console.log(mergeInventory([1,2],[3,4]))
+console.log(mergeInventory("1,2","3,4"))
+
+console.log(highestScore([3,7,2,9,4]))
+console.log(highestScore("3,7,2"))
+
