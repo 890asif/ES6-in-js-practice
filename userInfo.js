77bcfb445668d2b2f015e37b4679f1239104994e
890asif <192508892+890asif@users.noreply.github.com>
2026-08-05 00:23:00 +0600

    user info extract

---
+++ userInfo.js
@@ -0,0 +1,24 @@
+function extractUserInfo(userObj) {
+    if (!userObj.user?.name) {
+        return "Invalid";
+    }
+
+    const {
+        user: {
+            name: userName,
+            age: userAge
+        },
+        hobbies
+    } = userObj;
+
+    const [firstHobby = "nothing yet"] = hobbies;
+
+    return `${userName} (${userAge}) likes ${firstHobby}`;
+}
+
+console.log(
+    extractUserInfo({
+        user: { name: "Sadia", age: 22 },
+        hobbies: ["reading", "coding"]
+    })
+);
